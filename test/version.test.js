316d3b7feb6f4efbc3a9b2c62bf992d405a8e207
Dean Egan <deanoegan21@gmail.com>
2026-08-17 08:42:30 +0100

    Test v0.2 release metadata consistency

---
+++ test/version.test.js
@@ -0,0 +1,16 @@
+import assert from 'node:assert/strict';
+import fs from 'node:fs/promises';
+import test from 'node:test';
+import { fileURLToPath } from 'node:url';
+import { VERSION } from '../src/constants.js';
+
+const packagePath = fileURLToPath(new URL('../package.json', import.meta.url));
+const changelogPath = fileURLToPath(new URL('../CHANGELOG.md', import.meta.url));
+
+test('release metadata agrees on RuleTrip version', async () => {
+  const pkg = JSON.parse(await fs.readFile(packagePath, 'utf8'));
+  const changelog = await fs.readFile(changelogPath, 'utf8');
+  assert.equal(VERSION, '0.2.0');
+  assert.equal(pkg.version, VERSION);
+  assert.match(changelog, /\[0\.2\.0\]/u);
+});
